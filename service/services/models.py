c1fabb888e62c7fe851a8028b227d87dad349675
Sergey <fejanin@mail.ru>
2024-06-25 20:44:03 +0300

    Added new task using annotane.

--- service/services/models.py
+++ service/services/models.py
@@ -1,11 +1,22 @@
 from django.core.validators import MaxValueValidator
 from django.db import models
 from clients.models import Client
+from services.tasks import set_price
 
 
 class Service(models.Model):
     name = models.CharField(max_length=50)
     full_price = models.PositiveIntegerField()
+
+    def save(self, *args, **kwargs):
+        if self.__full_price != self.full_price:
+            for subscription in self.suscriptions.all():
+                set_price.delay(subscription.id)
+        return super().save(*args, **kwargs)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__full_price = self.full_price
 
     def __str__(self):
         return f'Service: {self.name}'
@@ -24,14 +35,25 @@ class Plan(models.Model):
                                                        MaxValueValidator(100)
                                                    ])
 
+    def save(self, *args, **kwargs):
+        if self.__discount_percent != self.discount_percent:
+            for subscription in self.suscriptions.all():
+                set_price.delay(subscription.id)
+        return super().save(*args, **kwargs)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__discount_percent = self.discount_percent
+
     def __str__(self):
         return f'{self.plan_type}'
 
 
 class Subscription(models.Model):
     client = models.ForeignKey(Client, related_name='suscriptions', on_delete=models.PROTECT)
-    service = models.ForeignKey(Service, related_name='suscriptions' , on_delete=models.PROTECT)
-    plan = models.ForeignKey(Plan, related_name='suscriptions' , on_delete=models.PROTECT)
+    service = models.ForeignKey(Service, related_name='suscriptions', on_delete=models.PROTECT)
+    plan = models.ForeignKey(Plan, related_name='suscriptions', on_delete=models.PROTECT)
+    price = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return f'{self.client} is subscribed to the service {self.service} according to the tariff plan {self.plan}'
